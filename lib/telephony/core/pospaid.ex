5dd98719be5bcd2b4ff843c6abaad2828e471d2d
Gustavo <g.92oliveira@gmail.com>
2022-11-01 07:26:25 -0300

    pospaid

--- lib/telephony/core/pospaid.ex
+++ lib/telephony/core/pospaid.ex
@@ -1,3 +1,22 @@
 defmodule Telephony.Core.Pospaid do
+  alias Telephony.Core.Call
   defstruct spent: 0
+  @price_per_minte 1.04
+
+  def make_call(subscriber, time_spent, date) do
+    subscriber
+    |> update_spent(time_spent)
+    |> add_call(time_spent, date)
+  end
+
+  defp update_spent(%{subscriber_type: subscriber_type} = subscriber, time_spent) do
+    spent = @price_per_minte * time_spent
+    subscriber_type = %{subscriber_type | spent: subscriber_type.spent + spent}
+    %{subscriber | subscriber_type: subscriber_type}
+  end
+
+  defp add_call(subscriber, time_spent, date) do
+    call = Call.new(time_spent, date)
+    %{subscriber | calls: subscriber.calls ++ [call]}
+  end
 end
