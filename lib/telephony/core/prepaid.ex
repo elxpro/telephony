46aa4c421e1add6da471cd64971cfc5c52df1ecd
Gustavo <g.92oliveira@gmail.com>
2022-11-01 07:08:09 -0300

    finish prepaid

--- lib/telephony/core/prepaid.ex
+++ lib/telephony/core/prepaid.ex
@@ -1,12 +1,42 @@
 defmodule Telephony.Core.Prepaid do
   alias Telephony.Core.Call
+  alias Telephony.Core.Recharge
   defstruct credits: 0, recharges: []
   @price_per_minute 1.45
 
   def make_call(%{subscriber_type: subscriber_type} = subscriber, time_spent, date) do
+    if is_subscriber_has_credits(subscriber_type, time_spent) do
+      subscriber
+      |> update_credit_spent(time_spent)
+      |> add_new_call(time_spent, date)
+    else
+      {:error, "Subscriber does not have credits"}
+    end
+  end
+
+  def make_recharge(%{subscriber_type: subscriber_type} = subscriber, value, date) do
+    recharge = Recharge.new(value, date)
+
+    subscriber_type = %{
+      subscriber_type
+      | recharges: subscriber_type.recharges ++ [recharge],
+        credits: subscriber_type.credits + value
+    }
+
+    %{subscriber | subscriber_type: subscriber_type}
+  end
+
+  defp is_subscriber_has_credits(subscriber_type, time_spent) do
+    subscriber_type.credits >= @price_per_minute * time_spent
+  end
+
+  defp update_credit_spent(%{subscriber_type: subscriber_type} = subscriber, time_spent) do
     credit_spent = @price_per_minute * time_spent
     subscriber_type = %{subscriber_type | credits: subscriber_type.credits - credit_spent}
-    subscriber = %{subscriber | subscriber_type: subscriber_type}
+    %{subscriber | subscriber_type: subscriber_type}
+  end
+
+  defp add_new_call(subscriber, time_spent, date) do
     call = Call.new(time_spent, date)
     %{subscriber | calls: subscriber.calls ++ [call]}
   end
