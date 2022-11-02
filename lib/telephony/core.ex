2d0bccaa2700a9cd0dcea1ac5ba5fb6d440c99ef
Gustavo <g.92oliveira@gmail.com>
2022-11-02 08:41:30 -0300

    finish the core

--- lib/telephony/core.ex
+++ lib/telephony/core.ex
@@ -17,4 +17,60 @@ defmodule Telephony.Core do
   def create_subscriber(_subscribers, _payload) do
     {:error, "Only 'prepaid' or 'postpaid' are accepted"}
   end
+
+  def search_subscriber(subscribers, phone_number) do
+    Enum.find(subscribers, &(&1.phone_number == phone_number))
+  end
+
+  def make_recharge(subscribers, phone_number, value, date) do
+    subscribers
+    |> search_subscriber(phone_number)
+    |> then(fn subscriber ->
+      if is_nil(subscriber) do
+        subscribers
+      else
+        subscribers = List.delete(subscribers, subscriber)
+        result = Subscriber.make_recharge(subscriber, value, date)
+        update_subcriber(subscribers, result)
+      end
+    end)
+  end
+
+  def make_call(subscribers, phone_number, time_spent, date) do
+    subscribers
+    |> search_subscriber(phone_number)
+    |> then(fn subscriber ->
+      if is_nil(subscriber) do
+        subscribers
+      else
+        subscribers = List.delete(subscribers, subscriber)
+        result = Subscriber.make_call(subscriber, time_spent, date)
+        update_subcriber(subscribers, result)
+      end
+    end)
+  end
+
+  def print_invoice(subscribers, phone_number, year, month) do
+    subscribers
+    |> search_subscriber(phone_number)
+    |> then(fn subscriber ->
+      if is_nil(subscriber) do
+        subscribers
+      else
+        Subscriber.print_invoice(subscriber, year, month)
+      end
+    end)
+  end
+
+  def print_invoices(subscribers, year, month) do
+    Enum.map(subscribers, &Subscriber.print_invoice(&1, year, month))
+  end
+
+  defp update_subcriber(subscribers, {:error, _message} = err) do
+    {subscribers, err}
+  end
+
+  defp update_subcriber(subscribers, subscriber) do
+    {subscribers ++ [subscriber], subscriber}
+  end
 end
